e9d9b3966981ed013427fc731f13f0bf5e1b401b
Bharath Sreevatsava <pedadabs@gmail.com>
2021-04-24 23:43:09 +0530

    api created

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-#2021-04-24
+from fastapi import FastAPI
+from covid import covid_analysis_module
+app = FastAPI()
+
+@app.get('/')
+def covid_analysis():
+    df = covid_analysis_module()
+    print(df)
+    return {"output" : df.to_dict('dict')}
